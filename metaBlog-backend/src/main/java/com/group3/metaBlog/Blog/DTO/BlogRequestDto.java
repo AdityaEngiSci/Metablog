d8759ee601346cd9011e7585b07201b5177ebfe1
kavankumar <150102479+KavanBrahmbhatt0910@users.noreply.github.com>
2024-07-22 18:15:11 -0300

    changed request dto for blog creation and changed response

--- metaBlog-backend/src/main/java/com/group3/metaBlog/Blog/DTO/BlogRequestDto.java
+++ metaBlog-backend/src/main/java/com/group3/metaBlog/Blog/DTO/BlogRequestDto.java
@@ -6,11 +6,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.Serializable;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class BlogRequestDto {
+public class BlogRequestDto implements Serializable {
     private String title;
     private String content;
     private MultipartFile image;
